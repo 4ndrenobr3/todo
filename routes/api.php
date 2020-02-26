898424256e4b75f41709425fe25e9c0590484882
4ndrenobr3 <nobre4ndr3@gmail.com>
2020-02-26 12:02:33 -0300

    Criado as rotas de login e registro, criado o controller do User

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Http\Request;
 
-Route::get("tasks", "TasksController@index");
-Route::get('tasks/{task}', 'TasksController@show');
-Route::post("tasks", "TasksController@store");
-Route::patch('tasks/{task}', 'TasksController@update');
-Route::delete('tasks/{task}', 'TasksController@destroy');
+Route::post('userlogin', 'UserController@userLogin');
+Route::post('userregister', 'UserController@userRegister');
+Route::group(['middleware' => 'auth:api'], function()
+{
+    Route::get("tasks", "TasksController@index");
+    Route::get('tasks/{task}', 'TasksController@show');
+    Route::post("tasks", "TasksController@store");
+    Route::patch('tasks/{task}', 'TasksController@update');
+    Route::delete('tasks/{task}', 'TasksController@destroy');
+});
